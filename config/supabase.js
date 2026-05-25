c9c9544a2791693180a94d413c3f58a285ccb7c6
iniyaekenebe92-afk <iniyaekenebe92@gmail.com>
2026-05-25 09:54:22 +0100

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,8 +1,18 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY
-);
+let _client = null;
 
-module.exports = supabase;
+function getClient() {
+  if (!_client) {
+    if (!process.env.SUPABASE_URL) throw new Error('SUPABASE_URL is not set');
+    if (!process.env.SUPABASE_SERVICE_KEY) throw new Error('SUPABASE_SERVICE_KEY is not set');
+    _client = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
+  }
+  return _client;
+}
+
+module.exports = new Proxy({}, {
+  get(_, prop) {
+    return getClient()[prop];
+  }
+});
